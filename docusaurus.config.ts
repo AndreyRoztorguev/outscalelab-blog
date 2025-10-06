e07dc9195f5b9e92a93c7b161863e4d10dc51802
Andrey <andrew.roztor99@gmail.com>
2025-10-06 16:59:10 +0300

    deploy fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,14 +17,14 @@ const config: Config = {
   url: "https://andreyroztorguev.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/outscalelab-blog/",
+  baseUrl: "outscalelab-blog",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "AndreyRoztorguev", // Usually your GitHub org/user name.
   projectName: "outscalelab-blog", // Usually your repo name.
-
   deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
 
